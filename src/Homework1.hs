0e1a7eac78c82aa4d2092b768612925ddb934628
Shane Xu <xusheng0711@gmail.com>
2017-07-03 23:37:15 +0800

    修改gard

--- src/Homework1.hs
+++ src/Homework1.hs
@@ -4,6 +4,7 @@ module Homework1
   , toDigits
   , sumDigits
   , validate
+  , hanoi
   ) where
 
 toDigitsRev :: Integer -> [Integer]
@@ -30,5 +31,6 @@ type Peg = String
 type Move = (Peg, Peg)
 
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi 0 _ _ _ = []
-hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
+hanoi n a b c
+  | n <= 0 = []
+  | otherwise = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
