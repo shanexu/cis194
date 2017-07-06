a2bc3945184b186529703dcbe1ef101aa0e4dc57
Shane Xu <xusheng0711@gmail.com>
2017-07-07 07:18:58 +0800

    solve haoni with 4pegs

--- src/Homework1.hs
+++ src/Homework1.hs
@@ -5,6 +5,8 @@ module Homework1
   , sumDigits
   , validate
   , hanoi
+  , hanoi1
+  , hanoi2
   ) where
 
 toDigitsRev :: Integer -> [Integer]
@@ -34,3 +36,22 @@ hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi n a b c
   | n <= 0 = []
   | otherwise = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n - 1) c b a
+
+hanoi1 :: Integer -> Integer
+hanoi1 n
+  | n <= 0 = 0
+  | otherwise = 2 * hanoi1 (n-1) + 1
+
+-- haoni with 4 pegs
+hanoi2 :: Integer -> Integer
+hanoi2 1 = 1
+hanoi2 2 = 3
+hanoi2 3 = 5
+hanoi2 4 = 9
+hanoi2 n
+  | n <= 0 = 0
+  | otherwise = f n
+  where
+    hk k = 2 * hanoi2 (n - k) + hanoi1 k
+    f 1 = hk 1
+    f k = min (hk k) (f (k - 1))
